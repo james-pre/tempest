42dca40537e30ad30d5d6e2716e0b65bae7e8758
dr-vortex <jp@drvortex.dev>
2023-11-29 14:51:28 -0600

    Added support for output files and formats to dump

--- src/cli/dump.cpp
+++ src/cli/dump.cpp
@@ -15,37 +15,51 @@ int main(int argc, char **argv)
 	try
 	{
 		options::options_description desc("Options");
-		desc.add_options()
-			("help,h", "Display help message")
-			("debug,d", "Show verbose/debug messages")
-			("file", "Input file");
+		desc.add_options()("help,h", "Display help message")("debug,d", "Show verbose/debug messages")("output,o", options::value<std::string>(), "Output file (for applicable output formats)")("format,f", options::value<std::string>()->default_value("text"), "Output format")("input", options::value<std::string>(), "Input file");
 		options::positional_options_description pos_desc;
-		pos_desc.add("file", 1);
+		pos_desc.add("input", 1);
 		options::variables_map vm;
 		options::store(options::command_line_parser(argc, argv).options(desc).positional(pos_desc).run(), vm);
 		options::notify(vm);
 
 		if (vm.count("help"))
 		{
-			std::cout << "Usage: <file>" << std::endl
+			std::cout << "Usage: <input>" << std::endl
 					  << desc << std::endl;
 			return 0;
 		}
 
-		if (!vm.count("file"))
+		if (!vm.count("input"))
 		{
 			throw std::runtime_error("No input file specified.");
 		}
 
-		std::string path = vm["file"].as<std::string>();
-		std::cout << "Dump of " << path << ":\n";
+		std::string path = vm["input"].as<std::string>();
+		std::string output = vm["output"].as<std::string>();
+		std::string format = vm["format"].as<std::string>();
+		if (format != "text" && output.empty())
+		{
+			throw std::runtime_error("No output file specified.");
+		}
 
-		File file = File::Read(path);
+		if (format == "gv" || format == "dot")
+		{
+			
+		}
 
-		std::cout << "Header: \n\tmagic: " << file.header.magic << "\n";
-		std::string type = (file.header.type < maxFileType) ? fileTypes.at(file.header.type) : "Unknown (" + std::to_string(file.header.type) + ")";
-		std::cout << "\ttype: " << type << "\n\tversion: " << std::to_string(file.header.version) << std::endl;
-		return 0;
+		if (format == "text")
+		{
+			std::cout << "Dump of " << path << ":\n";
+
+			File file = File::Read(path);
+
+			std::cout << "Header: \n\tmagic: " << file.header.magic << "\n";
+			std::string type = (file.header.type < maxFileType) ? fileTypes.at(file.header.type) : "Unknown (" + std::to_string(file.header.type) + ")";
+			std::cout << "\ttype: " << type << "\n\tversion: " << std::to_string(file.header.version) << std::endl;
+			return 0;
+		}
+
+		throw std::runtime_error("Format not supported");
 	}
 	catch (std::exception &err)
 	{
